909f9df462a9e2b48374dd334da141e24fb30865
jorvarea <jorvarea@c3r4s6.42malaga.com>
2024-08-09 21:07:22 +0200

    make list circular

--- philo/create_philosophers.c
+++ philo/create_philosophers.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 20:27:58 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/08/09 20:50:39 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/08/09 21:06:58 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ static void	init_philo(t_data *data, t_philo *curr_philo, t_philo *prev_philo,
 	// pthread_create(curr_philo->pthread, NULL, philo_routine, curr_philo);
 }
 
+void	make_list_circular(t_philo *philo_list, t_philo *curr_philo)
+{
+	curr_philo->next = philo_list;
+	philo_list->prev = curr_philo;
+}
+
 t_philo	*create_philosophers(t_data *data)
 {
 	t_philo			*philo_list;
@@ -38,7 +44,10 @@ t_philo	*create_philosophers(t_data *data)
 	{
 		curr_philo = (t_philo *)malloc(sizeof(t_philo));
 		if (!curr_philo)
+		{
+			printf("Error: Malloc failed\n");
 			return (NULL);
+		}
 		if (i == 0)
 		{
 			philo_list = curr_philo;
@@ -48,5 +57,6 @@ t_philo	*create_philosophers(t_data *data)
 		prev_philo = curr_philo;
 		i++;
 	}
+	make_list_circular(philo_list, curr_philo);
 	return (philo_list);
 }
