d9ed5021b555948eedcaee60e76929d5c51bd50d
jorvarea <jorvarea@users.noreply.github.com>
2024-08-10 20:20:44 +0200

    fix bad parsing

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 12:45:15 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/08/10 19:41:09 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/08/10 20:19:49 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	parse_input(t_data *data, char **args, int n_args)
 {
 	data->n_philo = (unsigned int)ft_atoi(args[0]);
 	data->time2die = (unsigned int)ft_atoi(args[1]);
-	data->time2die = (unsigned int)ft_atoi(args[2]);
+	data->time2eat = (unsigned int)ft_atoi(args[2]);
 	data->time2sleep = (unsigned int)ft_atoi(args[3]);
 	if (n_args == 5)
 		data->num_meals = (unsigned int)ft_atoi(args[4]);
