436a00ab98d06a100dab4739a856cc8a60b395a5
jorvarea <jorvarea@c3r4s6.42malaga.com>
2024-09-12 19:52:01 +0200

    fix for bonus

--- philo_bonus/leaks.sh
+++ philo_bonus/leaks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s -q ./philo $1 $2 $3 $4 $5
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s -q ./philo_bonus $1 $2 $3 $4 $5
